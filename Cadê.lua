09c1c7ea21e1a379cc18ad55c0f9d28714f256dd
josuekeliton-png <josuekeliton@gmail.com>
2026-08-01 12:06:10 -0300

    Csd.lua

--- Cadê.lua
+++ Cadê.lua
@@ -1,1 +1,7 @@
+-- ================================
+--           SCRIPT SITE           
+-- ================================
+print("Executando Script Site...")
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/huy384/redzHub/refs/heads/main/redzHub.lua"))()
 
